eca3b9c87e408cb4b30dbc3e26afdadd88c6111d
walljs <walljs@163.com>
2024-01-08 16:34:49 +0800

    clear

--- order-service/src/main/java/com/yg/cloud/order/mapper/OrderMapper.java
+++
@@ -1,9 +0,0 @@
-package com.yg.cloud.order.mapper;
-
-import com.yg.cloud.order.pojo.Order;
-import org.apache.ibatis.annotations.Select;
-
-public interface OrderMapper {
-    @Select("select * from t_order where id = #{id}")
-    Order findById(Long id);
-}
--- user-service/src/main/java/com/yg/cloud/user/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.yg.cloud.user.mapper;
-
-import com.yg.cloud.user.pojo.User;
-import org.apache.ibatis.annotations.Select;
-
-public interface UserMapper {
-    @Select("select * from t_user where id = #{userId}")
-    User findById(Long userId);
-}
